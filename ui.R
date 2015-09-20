6d01f47becc4f6441187fc674451f9f69a2d6384
hetianch <071cht@gmail.com>
2015-09-20 14:31:10 -0700

    reconnect

--- ui.R
+++ ui.R
@@ -1,14 +1,27 @@
 library(shiny) 
+require(markdown)
 
-shinyUI( pageWithSidebar(
-  # Application title 
-  headerPanel("BMI calculator"),
-  sidebarPanel(
-    numericInput('height', 'height cm', 162, min =130 , max = 250, step = 1), 
-    numericInput('weight', 'weight kg', 50, min =30 , max = 200, step = 1), 
-    submitButton('Submit')
-  ), mainPanel(
-    h3('Results:'),
-    h4('You BMI score is'), verbatimTextOutput("inputValue"), h4('You fall in the category of'), verbatimTextOutput("prediction")
-  ) ))
-  
+shinyUI( 
+  navbarPage("BMI calculator",
+             
+    tabPanel("application",
+      sidebarPanel(
+        numericInput('height', 'height cm', 162, min =130 , max = 250, step = 1), 
+        numericInput('weight', 'weight kg', 50, min =30 , max = 200, step = 1), 
+        submitButton('Submit')
+      ), 
+      
+      mainPanel(
+        h3('Results:'),
+        h4('You BMI score is'), verbatimTextOutput("inputValue"), h4('You fall in the category of'), verbatimTextOutput("prediction")
+      )
+      ),
+      
+    tabPanel("Documentation",
+    mainPanel(
+      includeMarkdown("about.md")
+      )
+    )
+    
+  )
+)
